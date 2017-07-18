639e9f5a24aeac8b3843bd100895f02a8b1b22e7
neiljoshua <neiljoshua@me.com>
2017-07-17 22:21:14 -0400

    home page

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	include('src/includes/header.php'); 
 ?>
 <body class="home">
-
+<img class="home-hero" src="/src/images/darkresort.jpg"; alt="Tarapoto Resorts">
 <a href="#" class="c-hamburger c-hamburger--rot"> <span>toggle menu</span> </a> 
 
 <h1 class="page-title">Tarapoto Resort</h1>	
