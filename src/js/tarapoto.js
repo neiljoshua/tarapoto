079e8ddda6e5be94e8840690587e0ebb039dac84
neiljoshua <neiljoshua@me.com>
2017-08-01 21:44:07 -0400

    adding dropkick.js

--- src/js/tarapoto.js
+++ src/js/tarapoto.js
@@ -1,5 +1,14 @@
 $( document ).ready( function(){
-	
+
+	$('.default').dropkick({
+        theme: 'dk-tarapoto',
+        monbile: 'true',
+        change: function (value, label) {
+          $(this).dropkick('theme', value);
+          console.log(value);
+        }
+      });
+
 	$('.c-hamburger').on('click', function(e){
 		e.preventDefault();
   		$(this).toggleClass('is-active');
