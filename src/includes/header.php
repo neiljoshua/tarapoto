2d60eeef7bf65f7b15aa393d8c8d42eab47a5c12
neiljoshua <neiljoshua@me.com>
2018-09-03 21:04:26 -0400

    Changing Theme color

--- src/includes/header.php
+++ src/includes/header.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="en-US">
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=0" >
+		<meta name="Description" content="Tarapoto Resort, A gem in the Peruvian Amazon">
+		<meta title="Tarapoto Resort">
+		<meta name="keywords" content="HTML,CSS,JavaScript">
+  	<meta name="author" content="Neil McGrath">
 		<title>Tarapoto Resort</title>
-		<meta name="viewport" content="initial-scale=1, maximum-scale=1">
-		<link rel="stylesheet" type="text/css" href="src/css/tarapoto.css">
+		<link rel="stylesheet" type="text/css" href="src/css/styles.css">
 		<link rel="stylesheet" type="text/css" href="src/css/plugins/dropkick.css">
 		<link rel="shortcut icon" href="/src/images/favicon.ico" type="image/x-icon" />
-		<script src="src/js/vendor/jquery-1.9.1.min.js"></script>
-		<script src="src/js/vendor/dropkick.2.1.9.min.js"></script>
-		<script src="src/js/tarapoto.js"></script>
+
 	</head>
 
