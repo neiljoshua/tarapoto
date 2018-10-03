0922bafd12768fc306e728be8e932726ca9e5d84
neiljoshua <neiljoshua@me.com>
2018-10-03 19:34:31 -0400

    Clean up

--- src/includes/header.php
+++ src/includes/header.php
@@ -1,29 +1,29 @@
 <!DOCTYPE html>
 <html lang="en-US">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=0" >
-		<meta name="Description" content="Tarapoto Resort, A gem in the Peruvian Amazon">
-		<meta title="Tarapoto Resort">
-		<meta name="keywords" content="HTML,CSS,JavaScript">
-  	<meta name="author" content="Neil McGrath">
-		<title>Tarapoto Resort</title>
-		<link rel="stylesheet" type="text/css" href="dist/css/styles.css">
-		<link rel="stylesheet" type="text/css" href="dist/css/plugins.css">
-		<link rel="shortcut icon" href="/src/images/favicon.ico" type="image/x-icon" />
-		<script src="dist/js/jquery.min.js"></script>
-		<script src="dist/js/vendor.min.js"></script>
-		<script src="dist/js/tarapoto.min.js"></script>
+  <head>
+    <title>Tarapoto Resort</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=0" >
+    <meta name="Description" content="Tarapoto Resort, A gem in the Peruvian Amazon">
+    <meta title="Tarapoto Resort">
+    <meta name="keywords" content="HTML,CSS,JavaScript">
+    <meta name="author" content="Neil McGrath">
+    <link rel="stylesheet" type="text/css" href="dist/css/styles.css">
+    <link rel="stylesheet" type="text/css" href="dist/css/plugins.css">
+    <link rel="shortcut icon" href="/src/images/favicon.ico" type="image/x-icon" />
+    <script src="dist/js/jquery.min.js"></script>
+    <script src="dist/js/vendor.min.js"></script>
+    <script src="dist/js/tarapoto.min.js"></script>
 
-	</head>
-	<body class="<?php echo $thisPage;?>">
-		<header>
-			<a href="/" class="logo">
-				<img class="logo__image" src="src/images/nazca_monkey_stroke.svg" alt="Tarapoto logo">
-			</a>
-			<a href="#" class="hamburger hamburger--rot"><span>toggle menu</span></a>
-			<?php
-				include('src/includes/menu.php');
-			?>
-		</header>
+  </head>
+  <body class="<?php echo $thisPage;?>">
+    <header>
+      <a href="/" class="logo">
+        <img class="logo__image" src="src/images/nazca_monkey_stroke.svg" alt="Tarapoto logo">
+      </a>
+      <a href="#" class="hamburger hamburger--rot"><span>toggle menu</span></a>
+      <?php
+        include('src/includes/menu.php');
+      ?>
+    </header>
 
--- src/includes/menu.php
+++ src/includes/menu.php
@@ -1,14 +1,14 @@
 <div class="menu">
-	<ul class="menu__list">
-		<li class="menu__item" <?php if ($thisPage=="location") echo " id=\"currentpage\""; ?>>
-			<a class="menu__link" href="location">LOCATION</a >
-		</li>
-		<li class="menu__item" <?php if ($thisPage=="history") echo " id=\"currentpage\""; ?>>
-			<a class="menu__link" href="history">HISTORY</a>
-		</li>
-		<li class="menu__item" <?php if ($thisPage=="inquiries") echo " id=\"currentpage\""; ?>>
-			<a class="menu__link" href="inquiries">INQUIRIES</a>
-		</li>
-	</ul>
-</div>	
-	
+  <ul class="menu__list">
+    <li class="menu__item" <?php if ($thisPage=="location") echo " id=\"currentpage\""; ?>>
+      <a class="menu__link" href="location">LOCATION</a >
+    </li>
+    <li class="menu__item" <?php if ($thisPage=="history") echo " id=\"currentpage\""; ?>>
+      <a class="menu__link" href="history">HISTORY</a>
+    </li>
+    <li class="menu__item" <?php if ($thisPage=="inquiries") echo " id=\"currentpage\""; ?>>
+      <a class="menu__link" href="inquiries">INQUIRIES</a>
+    </li>
+  </ul>
+</div>
+
